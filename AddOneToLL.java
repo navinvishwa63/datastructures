34f8b980dfe94f5ef61295ed8f79cbbbfe08c9a0
navinvishwa63 <60549351+navinvishwa63@users.noreply.github.com>
2020-06-24 19:09:27 +0530

    Given a linked list, add 1 to it
    Before: 1999
    After: 12000

---
+++ AddOneToLL.java
@@ -0,0 +1,59 @@
+package test.tree;
+
+public class AddOneToList {
+
+	static class Node {
+		int data;
+		Node next;
+
+		Node(int data) {
+			this.data = data;
+			next = null;
+		}
+
+	}
+
+	// return carry after adding 1 from end to start.
+	static int addWithCarry(Node node) {
+		if (node == null) {
+			return 1;
+		}
+
+		int res = node.data + addWithCarry(node.next);
+		node.data = res % 10;
+		res /= res;
+		return res;
+	}
+
+	static Node addOneToList(Node node) {
+		int carry = addWithCarry(node);
+		if (carry > 0) {
+			Node temp = new Node(carry);
+			temp.next = node;
+			return temp;
+
+		}
+		return node;
+	}
+
+	static void printList(Node node) {
+		while (node != null) {
+			System.out.print(node.data + "");
+			node = node.next;
+		}
+
+	}
+
+	public static void main(String args[]) {
+		Node head = new Node(1);
+		head.next = new Node(9);
+		head.next.next = new Node(9);
+		head.next.next.next = new Node(9);
+		System.out.print("Before: ");
+		printList(head);
+		System.out.print("\nAfter: ");
+		printList(addOneToList(head));
+
+	}
+
+}
