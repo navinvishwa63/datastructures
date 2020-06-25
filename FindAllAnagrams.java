6239018cc419be9250526299be2e13b9e2aa121e
navinvishwa63 <60549351+navinvishwa63@users.noreply.github.com>
2020-06-25 13:49:17 +0530

    Find starting indexes of all the anagrams

---
+++ FindAllAnagrams.java
@@ -0,0 +1,49 @@
+package test.tree;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class FindAllAnagrams {
+
+	public static List<Integer> findAnagram(String s, String p) {
+		int len = s.length();
+		int window = p.length();
+
+		List<Integer> result = new LinkedList<>();
+
+		// base condition
+		if (len < window)
+			return result;
+
+		int[] hash = new int[128];
+		int[] phash = new int[128];
+		int left = 0, right = 0;
+    
+    // we can use 'a' as well but use Math.abs(p.charAt(right) - 'A')
+		while (right < window) {
+			phash[p.charAt(right) - 'A'] += 1;
+			hash[s.charAt(right++) - 'A'] += 1;
+		}
+		right--;
+		while (right < len) {
+			if (Arrays.equals(phash, hash)) {
+				result.add(left);
+			}
+			right += 1;
+			if (right != len)
+				hash[s.charAt(right) - 'A'] += 1;
+			hash[s.charAt(left) - 'A'] -= 1;
+			left++;
+		}
+
+		return result;
+	}
+
+	public static void main(String args[]) {
+		 List<Integer> indexes_caps = findAnagram("ABCDEBACB", "BAC");
+		List<Integer> indexes = findAnagram("abcdebacb", "bac");
+		System.out.println(indexes);
+		System.out.println(indexes_caps);
+	}
+}
